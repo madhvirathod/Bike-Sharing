099850d6dbccb6fbedc1425ed30f746c989aed14
madhvirathod <mdvrathod1130@gmail.com>
2024-09-18 11:28:06 -0700

    new file added

--- .ipynb_checkpoints/st_dashboard_Part_2-checkpoint.py
+++ .ipynb_checkpoints/st_dashboard_Part_2-checkpoint.py
@@ -13,8 +13,8 @@ from PIL import Image
 ########################### Initial settings for the dashboard ####################################################
 
 
-st.set_page_config(page_title = 'Divvy Bikes Strategy Dashboard', layout='wide')
-st.title("Divvy Bikes Strategy Dashboard")
+st.set_page_config(page_title = 'NewYork Bikes Strategy Dashboard', layout='wide')
+st.title("NewYork Bikes Strategy Dashboard")
 
 # Define side bar
 st.sidebar.title("Aspect Selector")
@@ -35,15 +35,15 @@ top20 = pd.read_csv('top20.csv', index_col = 0)
 ### Intro page
 
 if page == "Intro page":
-    st.markdown("#### This dashboard aims at providing helpful insights on the expansion problems Divvy Bikes currently faces.")
-    st.markdown("Right now, Divvy bikes runs into a situation where customers complain about bikes not being available at certain times. This analysis will look at the potential reasons behind this. The dashboard is separated into 4 sections:")
+    st.markdown("#### This dashboard will help address the distribution and expansion problems that New York's Citi Bike service is currently facing.")
+    st.markdown("Currently, New York's bike-sharing service is experiencing issues with bike availability at certain stations and times. This analysis will look at the potential reasons behind this. The dashboard                  is separated into 4 sections:")
     st.markdown("- Most popular stations")
     st.markdown("- Weather component and bike usage")
     st.markdown("- Interactive map with aggregated bike trips")
     st.markdown("- Recommendations")
     st.markdown("The dropdown menu on the left 'Aspect Selector' will take you to the different aspects of the analysis our team looked at.")
 
-    myImage = Image.open("Divvy_Bikes.jpg") #source: https://ride.divvybikes.com/blog/new-divvy-ebike
+    myImage = Image.open("NewYork_Bikes.jpg") 
     st.image(myImage)
 
 
@@ -64,7 +64,7 @@ elif page == 'Weather component and bike usage':
     )
 
     fig_2.update_layout(
-    title = 'Daily bike trips and temperatures in NewYork 2022',
+    title = 'Daily bike trips and temperatures in 2022 NewYork',
     height = 400
     )
 
@@ -100,33 +100,33 @@ elif page == 'Most popular stations':
     width = 900, height = 600
     )
     st.plotly_chart(fig, use_container_width=True)
-    st.markdown("From the bar chart it is clear that there are some start stations that are more popular than others - in the top 3 we can see Streeter Drive/Grand Avenue, Canal Street/Adams Streat as well as Clinton Street/Madison Street. There is a big jump between the highest and lowest bars of the plot, indicating some clear preferences for the leading stations. This is a finding that we could cross reference with the interactive map that you can access through the side bar select box.")
+    st.markdown("From the bar chart it is clear that certain bike start stations are significantly more popular than others in New York. The top three stations are: W 21 St & 6 Ave, West St & Chambers St, and Broadway & W 56 St.There is a big jump between the highest and lowest bars of the plot, indicating some clear preferences for the leading stations. This is a finding that we could cross reference with the interactive map that you can access through the side bar select box.")
 
 elif page == 'Interactive map with aggregated bike trips': 
 
     ### Create the map ###
 
-    st.write("Interactive map showing aggregated bike trips over Chicago")
+    st.write("Interactive map showing aggregated bike trips over NewYork")
 
-    path_to_html = "Divvy Bike Trips Aggregated.html" 
+    path_to_html = "NewYork_Bike_Trip_map.html" 
 
     # Read file and keep in variable
     with open(path_to_html,'r') as f: 
         html_data = f.read()
 
     ## Show in webpage
-    st.header("Aggregated Bike Trips in Chicago")
+    st.header("Aggregated Bike Trips in NewYork")
     st.components.v1.html(html_data,height=1000)
     st.markdown("#### Using the filter on the left hand side of the map we can check whether the most popular start stations also appear in the most popular trips.")
     st.markdown("The most popular start stations are:")
-    st.markdown("Streeter Drive/Grand Avenue, Canal Street/Adams Street as well as Clinton Street/Madison Street. While having the aggregated bike trips filter enabled, we can see that even though Clinton Street/Madison Street is a popular start stations, it doesn't account for the most commonly taken trips.")
-    st.markdown("The most common routes (>2,000) are between Theater on the Lake, Streeter Dr/Grand Avenue, Millenium Park, Columbus Dr/Randolph Street, Shedd Aquarium, Michigan Avenue/Oak Street, Canal Street/Adams Street, which are predominantly located along the water.")
+    st.markdown("W 21 St & 6 Ave, West St & Chambers St, Broadway & W 56 St.Based on the visualization, it seems that the most popular start stations do align with popular bike trips, particularly in central areas like around Central Park and downtown Manhattan. The stations W 21 St & 6 Ave and West St & Chambers St appear to be located in areas where there is high trip activity, as indicated on the map.")
+    st.markdown("The most common bike routes are primarily centered in Manhattan, especially around major landmarks like Central Park, Columbia University, and NYU. There are also popular routes along the East River and Hudson River, connecting different parts of Manhattan and providing scenic rides.")
 
 else:
     
     st.header("Conclusions and recommendations")
-    bikes = Image.open("recs_page.png")  #source: Midjourney
+    bikes = Image.open("NewYork Bikes.jpg")  
     st.image(bikes)
-    st.markdown("### Our analysis has shown that Divvy Bikes should focus on the following objectives moving forward:")
-    st.markdown("- Add more stations to the locations around the water line, such as heater on the Lake, Streeter Dr/Grand Avenue, Millenium Park, Columbus Dr/Randolph Street, Shedd Aquarium, Michigan Avenue/Oak Street, Canal Street/Adams Street")
-    st.markdown("- Ensure that bikes are fully stocked in all these stations during the warmer months in order to meet the higher demand, but provide a lower supply in winter and late autumn to reduce logistics costs")
+    st.markdown("### Our analysis has shown that NewYork's bike-sharing service should focus on the following objectives moving forward:")
+    st.markdown("-The analysis shows that the most popular bike stations, particularly around W 21 St & 6 Ave and West St & Chambers St, coincide with high-density routes in central Manhattan, such as those near Central Park and NYU. There is a clear seasonal pattern where bike usage peaks during warmer months, with significantly fewer trips during the colder months.")
+    st.markdown("-To optimize operations, increase bike availability at popular stations and reduce stock during colder months (November to April) by 25-40%. Additionally, consider adding more stations along high-demand waterfront routes and implementing real-time restocking strategies to ensure bikes are always available at key stations.")
